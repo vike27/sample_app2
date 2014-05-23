5e49d55d41e82325040a3baf29751312fe4d09b7
Javier Noris <norisjavier@gmail.com>
2014-05-22 20:08:41 -0700

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
+describe User do
+
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
+  
+end
+
 describe "User pages" do
 
   subject { page }
